9d7afe6ce2b2281e947e85cb2597c3492660e0cc
Jérémy ACHAIN <achain.jeremy@gmail.com>
2023-04-15 10:05:37 +0200

    Prepare cordova

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+const bootstrap = () => {
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+};
+// @ts-ignore
+if (typeof window['cordova'] !== 'undefined') {
+  document.addEventListener('deviceready', () => {
+    bootstrap();
+  }, false);
+} else {
+  bootstrap();
+}
