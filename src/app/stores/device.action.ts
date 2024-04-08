df2a2d6857496dcfc1dad7f54839a0825a8341b0
Jaden ACHAIN <jaden.achain@gmail.com>
2024-04-08 13:40:25 +0200

    Test state-manager

--- src/app/stores/device.action.ts
+++ src/app/stores/device.action.ts
@@ -1,50 +1,58 @@
 import { DeviceBackInterface } from '@models';
+import { StateAction } from '@alkemist/ngx-state-manager';
 
-export class DeviceFillAction {
-  static readonly key = "Fill";
-
-  constructor(public payload: DeviceBackInterface[]) {
-  }
-}
-
-export class DeviceFilterAction {
-  static readonly key = "Filter";
+export class DeviceFillAction extends StateAction<DeviceBackInterface[]> {
+  static override readonly key = "Fill";
 
   constructor(public payload: DeviceBackInterface[]) {
+    super();
   }
 }
 
-export class DeviceGetAction {
-  static readonly key = "Get";
+export class DeviceFilterAction extends StateAction<DeviceBackInterface[]> {
+  static override readonly key = "Filter";
+
+  constructor(public payload: DeviceBackInterface[]) {
+    super();
+  }
+}
+
+export class DeviceGetAction extends StateAction<DeviceBackInterface> {
+  static override readonly key = "Get";
 
   constructor(public payload: DeviceBackInterface) {
+    super();
   }
 }
 
-export class DeviceAddAction {
-  static readonly key = "Add";
+export class DeviceAddAction extends StateAction<DeviceBackInterface> {
+  static override readonly key = "Add";
 
   constructor(public payload: DeviceBackInterface) {
+    super();
   }
 }
 
-export class DeviceUpdateAction {
-  static readonly key = "Update";
+export class DeviceUpdateAction extends StateAction<DeviceBackInterface> {
+  static override readonly key = "Update";
 
   constructor(public payload: DeviceBackInterface) {
+    super();
   }
 }
 
-export class DeviceDeleteAction {
-  static readonly key = "Delete";
+export class DeviceDeleteAction extends StateAction<DeviceBackInterface> {
+  static override readonly key = "Delete";
 
   constructor(public payload: DeviceBackInterface) {
+    super();
   }
 }
 
-export class DeviceResetAction {
-  static readonly key = "Reset";
+export class DeviceResetAction extends StateAction<void> {
+  static override readonly key = "Reset";
 
   constructor(public payload: void) {
+    super();
   }
 }
